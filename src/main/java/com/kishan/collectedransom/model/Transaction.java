6bb79c65af1209445550d35c237b381188bb97d7
Kishan B <bkishan@thoughtworks.com>
2017-05-19 10:57:40 +0530

    Transaction logging

--- src/main/java/com/kishan/collectedransom/model/Transaction.java
+++ src/main/java/com/kishan/collectedransom/model/Transaction.java
@@ -3,12 +3,14 @@ package com.kishan.collectedransom.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.math.BigInteger;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@ToString
 public class Transaction {
     private BigInteger total;
 }
